5b337126b09d6bf002db1e4a50779599ae6f3452
DESKTOP-U6KB5LI\march <marchettipm@gmail.com>
2017-10-07 10:04:28 -0400

    meh

---
+++ seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO clients (burgers) VALUES ('Dr. LoveBurger');
+INSERT INTO clients (burgers) VALUES ('Bacon Sutra');
+INSERT INTO clients (burgers) VALUES ('Four Meat Attack');
+INSERT INTO clients (burgers) VALUES ('Four Meat Attack');
+INSERT INTO clients (burgers) VALUES ('Four Meat Attack');
+
